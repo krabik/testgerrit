33236566af4c8df890007a950705b408ccd02938
tomash.wrzonski <tom.wrzonski@gmail.com>
2013-06-16 11:44:06 +0300

    add DemoClassLib project folder

---
+++ DemoClassLib/DemoClassLib/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoClassLib
+{
+    public class MyMathClass
+    {
+        public int sum(int i, int j)
+        {
+            return i + j;
+        }
+
+        //comment
+        public double multi(int i, int j)
+        {
+            return i * j;
+        }
+
+    }
+}
