2eb10df0eb4fa8e399b963982e06d436f9626494
tomash.wrzonski <tom.wrzonski@gmail.com>
2013-06-19 13:43:45 +0300

    add program main, add UT project

---
+++ DemoClassLib/Main/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DemoClassLib;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyMathClass m = new MyMathClass();
+
+            Console.WriteLine("Result: {0}", m.multi(5, 6));
+            Console.WriteLine("Result: {0}", m.sum(2, 2));
+            Console.WriteLine("Result: {0}", m.sub(3, 1));
+            Console.WriteLine();
+            Console.WriteLine("Hit 'Enter' lo leave...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DemoClassLib/TestProjectUT/MyMathClassTest.cs
@@ -0,0 +1,125 @@
+﻿using DemoClassLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProjectUT
+{
+    
+    
+    /// <summary>
+    ///This is a test class for MyMathClassTest and is intended
+    ///to contain all MyMathClassTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class MyMathClassTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for MyMathClass Constructor
+        ///</summary>
+        [TestMethod()]
+        public void MyMathClassConstructorTest()
+        {
+            MyMathClass target = new MyMathClass();
+            //Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for multi
+        ///</summary>
+        [TestMethod()]
+        public void multiTest()
+        {
+            MyMathClass target = new MyMathClass(); // TODO: Initialize to an appropriate value
+            int i = 2; // TODO: Initialize to an appropriate value
+            int j = 5; // TODO: Initialize to an appropriate value
+            double expected = 10; // TODO: Initialize to an appropriate value
+            double actual;
+            actual = target.multi(i, j);
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for sub
+        ///</summary>
+        [TestMethod()]
+        public void subTest()
+        {
+            MyMathClass target = new MyMathClass(); // TODO: Initialize to an appropriate value
+            int i = 0; // TODO: Initialize to an appropriate value
+            int j = 0; // TODO: Initialize to an appropriate value
+            int expected = 0; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.sub(i, j);
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for sum
+        ///</summary>
+        [TestMethod()]
+        public void sumTest()
+        {
+            MyMathClass target = new MyMathClass(); // TODO: Initialize to an appropriate value
+            int i = 2; // TODO: Initialize to an appropriate value
+            int j = 2; // TODO: Initialize to an appropriate value
+            int expected = 4; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.sum(i, j);
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
